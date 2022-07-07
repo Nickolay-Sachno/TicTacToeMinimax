8b972a4bbaa4ab66f87d543e3af6e0267e735422
Nickolay-Sachno <nickolay.sachno@is.com>
2022-07-07 18:16:43 +0300

    Deleted old files

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,3 @@
 fun main() {
     // By default, creates TicTacToe game, 3X3 grid with player 'X' plays first and agent 'O' second.
-    ManagerOld.play(GameStateOld())
 }
--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -1,5 +1,6 @@
 interface Player {
     val cellType : CellType
 
+    /** Return a coordinates (i,j) based on the game state */
     fun getNextMove(gameStateTest: GameStateTest) : Pair<Int,Int>
 }
